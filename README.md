9056ba7d829fa72d8d635adb0ec8675de84ca207
JoeyShapiro <joeyashapiro@gmail.com>
2025-01-31 21:39:44 -0600

    added bird

--- README.md
+++ README.md
@@ -8,15 +8,7 @@
 - Type the follow command:
 
 ```sh
-cmake -S . -B build
-```
-
-> if you want with debug symbols put the flag `-DCMAKE_BUILD_TYPE=Debug`
-
-- After CMake config your project build:
-
-```sh
-cmake --build build
+clang -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL lib/libraylib.a src/main.c -o Swallow
 ```
 
 - Inside the build folder are another folder (named the same as the project name on CMakeLists.txt) with the executable and resources folder.
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+#define SCALE 1
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -7,11 +9,24 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1024;
+    const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Swallow");
+    
+    // scale texture to 2x
+    Image img = LoadImage("res/BirdSprite.png");
+    ImageResize(&img, img.width * SCALE, img.height * SCALE);
+    Texture2D texture = LoadTextureFromImage(img);
 
+    Vector2 position = { 350.0f, 280.0f };
+    Vector2 position1 = { 350.0f, 400.0f };
+    Rectangle frameRec = { 0.0f, 0.0f, 16*SCALE, 16*SCALE };
+    Rectangle frameRec2 = { 0, 16*SCALE, 16*SCALE, 16*SCALE };
+    int currentFrame = 0;
+    int currentFrame2 = 0;
+
+    int framesCounter = 0;
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -20,15 +35,28 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        framesCounter++;
 
+        if (framesCounter >= (16))
+        {
+            framesCounter = 0;
+            currentFrame++;
+            currentFrame2++;
+
+            if (currentFrame > 1) currentFrame = 0;
+            if (currentFrame2 > 7) currentFrame2 = 0;
+
+            frameRec.x = (float)currentFrame*16*SCALE;
+            frameRec2.x = (float)currentFrame2*16*SCALE;
+        }
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-
+            DrawTextureRec(texture, frameRec, position, WHITE);
+            DrawTextureRec(texture, frameRec2, position1, WHITE);
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
@@ -37,6 +65,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    UnloadTexture(texture);
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
